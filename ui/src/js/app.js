570ff86b59dff89f9b77b12ee9865bdd3728040c
RubbieKelvin <rubbiekelvinvoltsman@gmail.com>
2019-11-18 14:19:06 +0100

    work on intel

--- ui/src/js/app.js
+++ ui/src/js/app.js
@@ -9,22 +9,22 @@ function reset_game(){
 	for (var i = 0; i < tic_buttons.length; i++) {
 		tic_buttons[i].unmark_card();
 	}
-	let current_player = player.computer;
+	let current_player = player.human;
 	Game.reset()
 }
 
 const play = (number) => {
 	if (Game.len_slot!=0){
-		// Game.player_pick(number, (current_player=="O"));
-		// current_player = player.computer;
-		Game.computer_pick((current_player=="O"));
+		Game.player_pick(number, (current_player=="O"));
 		current_player = player.computer;
+		// Game.computer_pick((current_player=="O"));
+		// current_player = player.computer;
 
 		if (Game.len_slot!=0){
-			// Game.computer_pick((current_player=="O"));
-			// current_player = player.human;
-			Game.player_pick(number, (current_player=="O"));
+			Game.computer_pick((current_player=="O"));
 			current_player = player.human;
+			// Game.player_pick(number, (current_player=="O"));
+			// current_player = player.human;
 			if (Game.len_slot==0){
 				// reset_game();
 			}
